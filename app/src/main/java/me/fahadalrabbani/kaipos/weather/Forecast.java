3ebc1905a88e081f7377df7b89d8442e637eb727
Fahad Al Rabbani <alfahad1993@gmail.com>
2015-08-31 14:44:08 +0100

    getters and setters for forecast.

--- app/src/main/java/me/fahadalrabbani/kaipos/weather/Forecast.java
+++ app/src/main/java/me/fahadalrabbani/kaipos/weather/Forecast.java
@@ -4,7 +4,32 @@ package me.fahadalrabbani.kaipos.weather;
  * Created by achernar on 31/08/15.
  */
 public class Forecast {
+    
     private Current mCurrent;
     private Hour[] mHourlyForecast;
     private Day[] mDailyForecast;
+
+    public Current getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(Current current) {
+        mCurrent = current;
+    }
+
+    public Hour[] getHourlyForecast() {
+        return mHourlyForecast;
+    }
+
+    public void setHourlyForecast(Hour[] hourlyForecast) {
+        mHourlyForecast = hourlyForecast;
+    }
+
+    public Day[] getDailyForecast() {
+        return mDailyForecast;
+    }
+
+    public void setDailyForecast(Day[] dailyForecast) {
+        mDailyForecast = dailyForecast;
+    }
 }
